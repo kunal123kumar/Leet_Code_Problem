0a3fea1a458b5e56bf9bda54a9f3628ca309cde3
KUNAL KUMAR <154955493+kunal123kumar@users.noreply.github.com>
2026-07-24 09:20:06 +0530

    Delete 607-sales-person directory

--- 607-sales-person/sales-person.sql
+++
@@ -1,3 +0,0 @@
-# Write your MySQL query statement below
-select name from salesPerson where sales_id NOT IN (select  o.sales_id from Company c 
- join  Orders  o on c.com_id = o.com_id where c.name = 'red');
